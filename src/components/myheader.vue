02beb2785f2bbdd386bf0b3125d1843fdb94f503
kadhum alrubaye <kadhum.it@gmail.com>
2023-09-24 16:17:52 +0300

    header design with elements done

--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -14,10 +14,16 @@
             Zain Iraq Digital & oodi's <br>
             Qurterly Newsletter
         </h1>
+
         <section >
             <img id="header_ellips" :src="header_ellips" alt="big start" class=" img-fluid">
             <img id="header_green_ball" :src="header_green_ball" alt=" header_green_ball" class=" img-fluid">
             <img id="header_cutted_circule" :src="header_cutted_circule" alt=" header_cutted_circule" class=" img-fluid">
+            <img id="header_white_half_circul" :src="header_white_half_circul" alt="header_white_half_circul" class=" img-fluid">
+            <img id="header_small_green_half_circle" :src="header_small_green_half_circle" alt="header_small_green_half_circle" class=" img-fluid">
+            <img id="header_white_dots" :src="header_white_dots" alt="header_white_dots" class=" img-fluid">
+            <img id="header_white_start_series" :src="header_white_start_series" alt="header_white_start_series" class=" img-fluid">
+
 
            <div id="TheDigitalInsider" class=" container">
             <h2>
@@ -33,6 +39,10 @@
             
            </div>
         </section>
+        <img id="zain_logo" :src="zain_logo" alt="zain_logo" class=" img-fluid">
+       <span>&</span>
+        <img id="header_oodi_logo" :src="header_oodi_logo" alt="header_oodi_logo" class=" img-fluid">
+
     </header>
 </template>
   
@@ -45,6 +55,12 @@ import header_small_star from '@/assets/img/header/header_small_star.png'
 import header_ellips from '@/assets/img/header/header_ellips.png'
 import header_green_ball from '@/assets/img/header/header_green_ball.png'
 import header_cutted_circule from '@/assets/img/header/header_cutted_circule.png'
+import header_small_green_half_circle from '@/assets/img/header/header_small_green_half_circle.png'
+import header_white_half_circul from '@/assets/img/header/header_white_half_circul.png'
+import header_white_dots from '@/assets/img/header/header_white_dots.png'
+import header_white_start_series from '@/assets/img/header/header_white_start_series.png'
+import zain_logo from '@/assets/img/header/zain_logo.png'
+import header_oodi_logo from '@/assets/img/header/header_oodi_logo.png'
 
 
 
@@ -59,6 +75,12 @@ export default {
             header_ellips,
             header_green_ball,
             header_cutted_circule,
+            header_small_green_half_circle,
+            header_white_half_circul,
+            header_white_dots,
+            header_white_start_series,
+            zain_logo,
+            header_oodi_logo,
         }
 
     },
@@ -89,8 +111,7 @@ header {
 }
 
 #header_green_start {
-    max-width: 150px;
-    max-height: 345px;
+ 
     position: absolute;
     top: 30%;
     right: -1px
@@ -98,9 +119,9 @@ header {
 
 #header_small_star {
     position: absolute;
-    top: 60%;
+    top: 61%;
     right: -1px;
-    margin-right: 10%;
+    margin-right: 12%;
 
 
 }
@@ -142,11 +163,7 @@ section {
 #header_ellips {
 
     max-width: 559.08px;
-
     position: absolute;
-   
-
-
 }
 
 /* the */
@@ -193,6 +210,47 @@ h4 {
   top:75px;
   z-index: 0; 
 }
-
+#header_small_green_half_circle{
+    width: 12%;
+    position: absolute; 
+    top:2.5rem;
+    right: 8.5rem;        
+    
+}
+#header_white_half_circul{
+    position: absolute; 
+    top:4rem;
+    right: 6rem;
+}
+#header_white_dots{
+    position: absolute;
+    bottom: 10rem;
+    left: 1rem;
+}
+#header_white_start_series{
+   position: absolute;
+   right: 1rem;
+   top:-2rem;
+}
+#zain_logo{
+    position: absolute;
+    top: 83%;
+    
+    margin-left: 5rem;
+}
+#header_oodi_logo{
+    position: absolute;
+    top: 82%;
+    
+    margin-left: 25rem;
+}
+span{
+    color: white;
+    font-size: 3rem;
+    position: absolute;
+    top: 83%;
+    font-style: italic;
+    margin-left: 22rem; 
+}
 </style>  
 <style></style>
