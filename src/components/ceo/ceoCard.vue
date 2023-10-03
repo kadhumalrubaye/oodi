18a44c077b6fbaa1bcb09db323bc162418b13417
kadhumalarubaye <kadhum.it@gmail.com>
2023-10-03 20:48:22 +0300

    fix ceo speech text overflow outside column

--- src/components/ceo/ceoCard.vue
+++ src/components/ceo/ceoCard.vue
@@ -1,16 +1,15 @@
 <template  >
-  <div v-for="(ceo, index) in ceos" :key="index" class="      col-sm-12 col-md-6 col-lg-6 col-xl-6 card-deck d-flex flex-warp    ">
+  <div v-for="(ceo, index) in ceos" :key="index" class="      col-sm-12 col-md-6 col-lg-6 col-xl-6 card-deck d-flex ">
     <div class="card text-center  ">
-      <img class="card-img-top img-fluid align-center text-center  align-self-center " :src="ceo.img"
-        alt="Card image cap">
+      <img class="card-img-top img-fluid align-self-center " :src="ceo.img" alt="Card image cap">
 
-      <div class="card-body     ">
+      <div class="card-body">
         <h5 class="card-title" :style="{ color: 'white' }">{{ ceo.name }}</h5>
         <h5 class="card-title">{{ ceo.position }}</h5>
         <p class="card-text  ">
           {{ ceo.msg }}
         </p>
-        <footer class="card-text p-3  ">
+        <footer class="card-text  ">
           {{ ceo.footer }}
         </footer>
 
@@ -67,7 +66,7 @@ export default {
 
 
 .card {
-  height: 1500px;
+
   background-color: #5B0AD9;
   border-radius: 200px;
   padding-bottom: 10%;
@@ -78,6 +77,7 @@ export default {
 .card-text {
   color: white;
   font-size: 29.17px;
+  white-space: pre-line;
 
 }
 
--- src/models/employee.ts
+++ src/models/employee.ts
@@ -19,7 +19,7 @@ const DanielToranModel: EmployeeModel = new EmployeeModel();
 DanielToranModel.name = "Daniel Toran"
 DanielToranModel.position = "Chief Digital Officer";
 DanielToranModel.msg = DanielToran
-DanielToranModel.footer = "Let us know what youthink \n digital@iq.zain.com "
+DanielToranModel.footer = "Let us know what you think \n digital@iq.zain.com "
 
 
 
